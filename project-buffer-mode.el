68880e58918a3dbcaa33624d4941cb85cebb5375
Cedric Lallain <kandjar76@hotmail.com>
2009-01-21 21:07:00 -0800

    Update msvc -- added some test code

--- project-buffer-mode.el
+++ project-buffer-mode.el
@@ -81,7 +81,7 @@
 ;; - Different coler for files referenced in the proj but don't exist?
 ;; - Auto reload if file modified on disk?
 ;;
-
+(require 'ewoc)
 
 
 (defvar project-buffer-status nil)
@@ -104,12 +104,19 @@
 ;;
 
 (defun project-buffer--prettyprint(data)
-  "Pretty-printer function"
-  
- )
+  "Pretty-printer function"		; assume it's currently just a cons (project . filename)
+  (if (or (not (cdr data))
+	  project-buffer-show-hidden
+	  (/= (car (string-to-list (cdr data))) ??))
+      (insert (concat "   "
+		      (if (cdr data) "     `- " "[-] ")
+		      (or (cdr data) (car data))
+		      "\n"
+		      ))))
 
 (defun project-buffer-mode()
   "Entry point to the project-buffer-mode"
+  (interactive)
   (kill-all-local-variables)
   (buffer-disable-undo)
   (setq mode-name "project-buffer"
@@ -117,10 +124,71 @@
 	buffer-read-only t)
   (let ((buffer-read-only nil))
     (erase-buffer)
-    (let ((status (ewoc-create 'project-buffer--prettyprint "" "")))
+    (let ((status (ewoc-create 'project-buffer--prettyprint "" "" t)))
       (make-local-variable 'project-buffer-status)
-      (setq project-buffer-status status))))
+      (make-local-variable 'project-buffer-show-hidden) ;; tmp hack
+      (setq project-buffer-show-hidden nil)
+      (setq project-buffer-status status)
+      (project-buffer-refresh-ewoc-hf status))))
 
+(defun project-buffer-show-hidden()
+  (interactive)
+  (setq project-buffer-show-hidden (not project-buffer-show-hidden))
+  (project-buffer-refresh-ewoc-hf project-buffer-status)
+  (project-buffer-refresh-nodes project-buffer-status)
+)
+
+(defun project-buffer-refresh-ewoc-hf(status)
+  "Refresh ewoc header/footer"
+  (ewoc-set-hf status 
+	       (concat "Booh Header\n"
+		       "Showing Hidden File: " (or (and project-buffer-show-hidden "shown") "hidden")
+		       "\n\n") ""))
+
+(defun project-buffer-refresh-nodes(status)
+  "Refresh displayed buffer"
+  (ewoc-map (lambda (data) (if (cdr data) t))
+	    status
+	    ))
+  
+;;(ewoc-data (ewoc-locate project-buffer-status))
+;;(ewoc-invalidate project-buffer-status pos)
+;;(ewoc-goto-prev project-buffer-status 1)))
+;;(ewoc-goto-next project-buffer-status 1)))
+
+
+(defun project-buffer-insert (status data) ; same as pretty print, assume data is cons (project . filename)
+  "Insert a file at the right place in it's project."
+  (let ((node          (ewoc-nth status 0))
+	(node-data     nil)
+	(here          nil) 
+	(project-found nil)
+	(prev          nil))
+    (while (and node (not here))
+      (setq node-data (ewoc-data node))
+      (cond
+       ;; data.project < node.project -> insert here...
+       ((string-lessp (car data) (car node-data))
+	(setq here node))
+
+       ;; node.project == data.project -> check file name
+       ((string-equal (car node-data) (car data))
+	(when (and (cdr node-data)
+		   (string-lessp (cdr data) (cdr node-data)))
+	  (setq here node))
+	(setq project-found t)))
+      ;; Carry on...
+      (setq prev node
+	    node (ewoc-next status node)))
+    ;; Insert before here...
+    (if (not project-found)
+	(if here 
+	    (ewoc-enter-before status here (cons (car data) nil))
+	    (ewoc-enter-last status (cons (car data) nil))))
+    (when (cdr data)
+      (if here 
+	  (ewoc-enter-before status here data)
+	  (ewoc-enter-last status data)))))
 
 
 (defun create-project-buffer(bufname)
