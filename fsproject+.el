18842275a2d888070204bc4804260ed49b503b29
Cedric Lallain <kandjar76@hotmail.com>
2009-08-19 01:03:56 -0700

    fsproject-add-project now query the user for its multiple inputs.

--- fsproject+.el
+++ fsproject+.el
@@ -1,4 +1,4 @@
-;;; fsproject.el --- File System Project Viewer
+;;; fsproject+.el --- File System Project Viewer
 ;;
 ;; Author:      Cedric Lallain <kandjar76@hotmail.com>
 ;; Version:     1.0
@@ -52,40 +52,160 @@
     (java      ("\\.java$" "\\.js$"))
     (cg        ("\\.cg\\(?:fx\\)?$"))
     (web       ("\\.htm\\(?:l\\)?$" "\\.xml" "\\.php$" "\\.js$" "\\.css$"))
-))
+    (custom    (nil)))
+  "List of the different file filters."
+)
 
 (defcustom fsprojectp-project-type
-  '((makefile ("\\.mak$" "Makefile$"))
-    (cmake    ("CMakeLists.txt"))
-    (jam      ("Jamfile\\(?:s\\)?$" "Jambase$" "Jamroot$"))
-    (scons    ("SConstruct$" "Sconscript$"))
-    (dmconfig ("build.dmc"))
-))
+  '((makefile ("\\.mak$" "Makefile$")     
+	      ("make CONFIG={build}" "make clean CONFIG={build}" "" ""))
+    (cmake    ("CMakeLists.txt")                         
+	      ("make CONFIG={build}" "make clean CONFIG={build}" "" ""))
+    (jam      ("Jamfile\\(?:s\\)?$" "Jamrules$" "Jambase$" "Jamroot$")
+	      ("jam -a {project}" "jam clean -a {project}" "" ""))
+    (scons    ("SConstruct$" "Sconscript$")
+	      ("scons" "scons --clean" "" ""))
+    (dmconfig ("build.dmc$")
+	      ("make {platform}.{project}-{config}.build"
+	       "make {platform}.{project}-{config}.clean"
+	       "make {platform}.{project}-{config}.run"
+	       "make {platform}.{project}-{config}.debug"))
+    (cabal    ("\\.cabal$")
+	      ("cabal build" "cabal clean" "" ""))
+    (any      (".*$")
+	      ("" "" "" ""))
+    (blank    nil
+	      ("" "" "" "")))
+  "List of the different project type.
+
+Each project type is a list of the following format:
+  (symbol matching-regexp (action-string-list)) where
+  action-string-list is a set of 4 strings representing the default
+  command to 'build' 'clean' 'run' and 'debug'.
+  the following wild cards can be use in each action string:
+   {config}   the current selected build version
+   {platform} the current selected platform 
+   {project}  name of the project
+   {path}     path of the project"
+)
 
 (defcustom fsprojectp-ignore-folder
-  '(".git" ".svn" "bzr" ".hg" "CVS" ".CVS" "build" "Debug" "Release"))
+  '(".git" ".svn" "bzr" ".hg" "CVS" ".CVS" "build" "lib" "Debug" "Release")
+  "List of folder to ignore during the recursive search.")
+
+;;
+;;  Local variables:
+;;
+
+(defvar fsprojectp-last-project-type-choosen "makefile")
+(defvar fsprojectp-last-filter-type-choosen "c++")
+(defvar fprojectp-last-file-filter-query-mode-choosen "regexp")
+(defvar fprojectp-last-file-filter-regexp-choosen nil)
+(defvar fprojectp-last-file-extension-list-choosen nil)
+
 
 ;;
 ;;  Functions:
 ;;
 
+(defun fsprojectp-choose-project-type()
+  "Request and return the selected project type"
+  (let* ((project-type-string (completing-read (format "Project Type [default %s]: " fsprojectp-last-project-type-choosen)
+					       fsprojectp-project-type nil t nil nil fsprojectp-last-project-type-choosen))
+	 (project-type (intern project-type-string)))
+    (setq fsprojectp-last-project-type-choosen project-type-string)
+    (assoc project-type fsprojectp-project-type)))
 
-(defun fsprojectp-read-filter()
+(defun fprojectp-shorten-string(str max-lgt)
+  "If the length of STR is greater than MAX-LGT; shorten the string adding '...' at the end."
+  (if (> (length str) max-lgt)
+      (concat (substring str 0 (- max-lgt 3)) "...")
+      str))
+
+(defun fsprojectp-choose-file-filter()
   "Read the file filter."
-  (interactive)
-  (
-  )
+  (let* ((filter-type-string (completing-read (format "Filter Type [default %s]: " fsprojectp-last-filter-type-choosen)
+					       fsprojectp-filters nil t nil nil fsprojectp-last-filter-type-choosen))
+	 (filter-type (intern filter-type-string)))
+    (setq fsprojectp-last-filter-type-choosen filter-type-string)
+    (if (not (eq filter-type 'custom))
+	;; If not custom: return the selected file-filter:
+	(assoc filter-type fsprojectp-filters)
+	;; In case of custom file filter:
+	;; Let's first ask how to specify the filter:
+	(let* ((query-mode-string (completing-read (format "Enter the file system query mode (regexp, file-extension) [default %s]: " fprojectp-last-file-filter-query-mode-choosen)
+						   '("regexp" "file-extension") nil t nil nil fprojectp-last-file-filter-query-mode-choosen))
+	       (query-mode (intern query-mode-string)))
+	  (setq fprojectp-last-file-filter-query-mode-choosen query-mode-string)
+	  (cond ((eq query-mode 'regexp)
+		 ;; A regexp: 
+		 (let* ((def-string (if fprojectp-last-file-filter-regexp-choosen
+					(concat " [default " (fprojectp-shorten-string fprojectp-last-file-filter-regexp-choosen 9) "]")
+					""))
+			(file-filter-regexp (read-from-minibuffer (format "Enter the file filter regexp%s: " def-string))))
+		   (if (= (length file-filter-regexp) 0)
+		       (setq file-filter-regexp fprojectp-last-file-filter-regexp-choosen)
+		       (setq fprojectp-last-file-filter-regexp-choosen file-filter-regexp))
+		   (list 'custom (list file-filter-regexp))))
+		((eq query-mode 'file-extension)
+		 ;; A list of file extension:
+		 (let* ((def-string (if fprojectp-last-file-extension-list-choosen
+					(concat " [default " (fprojectp-shorten-string fprojectp-last-file-extension-list-choosen 9) "]")
+					""))
+			(file-extension-list (read-from-minibuffer (format "Enter the list of extension separated by spaces%s: " def-string))))
+		   (if (= (length file-extension-list) 0)
+		       (setq file-extension-list fprojectp-last-file-extension-list-choosen)
+		       (setq fprojectp-last-file-extension-list-choosen file-extension-list))
+		   (list 'custom (list (concat "\\." (regexp-opt (split-string file-extension-list)) "$")))))
+		(t (error "Unknown Query Mode")))))))
 
 
-(defun fsprojectp-add-project(folder main-file filter)
+;;
+;;  User command:
+;;
+
+
+(defun fsprojectp-add-project(&optional project-type project-main-file project-root-folder file-filter)
   "Select a FOLDER, a MAIN-FILE and a FILE-FILTER, then add all
 files under the current folder and sub-folder matching the
 FILE-FILTER will be added to the project."
-  (interactive (list (read-directory-name "Project Root Folder: " nil nil t)
-		     (read-file-name "Project Main File: " nil nil t)
-		     (fsprojectp-read-filter)))
+  (interactive)
+  (when (interactive-p)
+    ;; Read the project-type
+    (unless project-type
+      (setq project-type (fsprojectp-choose-project-type)))
+    ;; Read the project-main-file (if the project's type is 'blank' there is no root filename)
+    (unless project-main-file
+      (when (nth 1 project-type)
+	(let* ((project-filter (nth 1 project-type))
+	       (project-predicate (lambda (filename)
+				    (and (not (string-equal filename "./"))
+					 (not (string-equal filename "../"))
+					 (or (file-directory-p filename)
+					     (some '(lambda (item) (string-match item filename)) project-filter))))))
+	  (while (or (not project-main-file)
+		     (file-directory-p project-main-file)
+		     (not (funcall project-predicate project-main-file)))
+	    (let ((def-dir (and project-main-file (file-directory-p project-main-file) project-main-file)))
+	      (setq project-main-file (read-file-name "Project Main File: " def-dir nil t nil project-predicate))
+	      (message "pmf: %s" project-main-file)
+	      )))))
+    ;; Read the project-root-folder:
+    (unless project-root-folder
+      (let ((def-dir (if project-main-file
+			 (file-name-directory project-main-file)
+			 default-directory)))
+	(while (or (not project-root-folder)
+		   (= (length project-root-folder) 0))
+	  (setq project-root-folder (read-directory-name "Project Root Folder: " def-dir def-dir t)))))
+    ;; Read the file-filter:
+    (unless file-filter
+      (setq file-filter (fsprojectp-choose-file-filter)))
+    )
+  ;;
+  ;;
+  ;;
   )
-	       
 
 
 (defun fsprojectp-setup-local-key()
