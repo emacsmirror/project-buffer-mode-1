f567540fa68aad969e793703259b52ffc9554b05
Tom Breton (Tehom) <tehom@panix.com>
2010-09-23 21:02:19 -0400

    Added an example project for test purposes

---
+++ project-buffer-mode/examples/project-1/file1.el
@@ -0,0 +1,4 @@
+;;;_ file1.el --- An example file in project-1
+;;;_. Footers
+;;;_ , End
+;;; file1.el ends here
---
+++ project-buffer-mode/examples/project-1/file2.el
@@ -0,0 +1,5 @@
+;;;_ file2.el --- An example file in project-1
+
+;;;_. Footers
+;;;_ , End
+;;; file2.el ends here
