b40b163a18024015ac4f8bda25f43892978a2277
Tom Breton (Tehom) <tehom@panix.com>
2010-09-24 21:40:37 -0400

    Reverting "Added an example project for test purposes" - moved into branch "test-project"

--- project-buffer-mode/examples/project-1/file1.el
+++
@@ -1,4 +0,0 @@
-;;;_ file1.el --- An example file in project-1
-;;;_. Footers
-;;;_ , End
-;;; file1.el ends here
--- project-buffer-mode/examples/project-1/file2.el
+++
@@ -1,5 +0,0 @@
-;;;_ file2.el --- An example file in project-1
-
-;;;_. Footers
-;;;_ , End
-;;; file2.el ends here
